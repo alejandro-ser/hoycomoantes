992b7ae79eb5eebb3d3f9281aa1116846e48d401
Alejo Oo <alejosegi@gmail.com>
2019-10-30 03:49:12 -0500

    Se actualizo el layout, se creo menu, descripcion, pelicula, se subieron videos y audio

--- js/main.js
+++ js/main.js
@@ -1,14 +1,53 @@
-var video = document.getElementById("video");
-var btn = document.getElementById("btn-control");
+// var video = document.getElementById("video");
+//
+// function myFunction() {
+//   if (video.paused) {
+//     video.play();
+//     document.getElementById("btn-icon").classList.remove('fa-play');
+//     document.getElementById("btn-icon").classList.add('fa-pause');
+//   } else {
+//     video.pause();
+//     document.getElementById("btn-icon").classList.remove('fa-pause');
+//     document.getElementById("btn-icon").classList.add('fa-play');
+//   }
+// }
 
-function myFunction() {
-  if (video.paused) {
-    video.play();
-    document.getElementById("btn-icon").classList.remove('fa-play');
-    document.getElementById("btn-icon").classList.add('fa-pause');
-  } else {
-    video.pause();
-    document.getElementById("btn-icon").classList.remove('fa-pause');
-    document.getElementById("btn-icon").classList.add('fa-play');
-  }
+// Menu
+
+// Get Modal
+var modal = document.getElementById('modalMenu');
+
+// Get pseudoelement to open Modal
+var btnMenu = document.getElementById("menu");
+
+// Get the <span> element to close Modal
+var back = document.getElementById("volver");
+
+// When user clicks button, open Modal
+btnMenu.onclick = function() {
+   modal.style.display = "block";
+   };
+
+// When user clicks Close (x), close Modal
+back.onclick = function() {
+   modal.style.display = "none";
+   };
+
+// When user clicks anywhere outside of the Modal, close Modal
+ window.onclick = function(event) {
+    if (event.target == modal) {
+       modal.style.display = "none";
+       }
+    }
+
+
+// Pelicula
+function showInfo(id) {
+  info = document.getElementById(id)
+  info.style.display = "block";
+}
+
+function hideInfo(id) {
+  info = document.getElementById(id)
+  info.style.display = "none";
 }
